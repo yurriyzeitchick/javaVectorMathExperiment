1900d0831e21b218aee3bc15c285374e56dcc679
yurriyzeitchick <yuriy.zaychyk@dxc.com>
2022-09-06 21:29:11 +0300

    Adding license disclaimer

--- src/main/java/com/luxoft/yz/GpuOpenclLengthCalc.java
+++ src/main/java/com/luxoft/yz/GpuOpenclLengthCalc.java
@@ -1,3 +1,12 @@
+/* This file is a part of javaVectorMathExperiment.
+ * javaVectorMathExperiment is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.luxoft.yz;
 
 import com.aparapi.Kernel;
@@ -37,9 +46,7 @@ public class GpuOpenclLengthCalc
         };
 
         kernel.execute(Range.create(x2.length));
-        //var sb = new StringBuilder();
-        //KernelManager.instance().reportDeviceUsage(sb, true);
-        //System.out.println(sb);
+
         kernel.dispose();
         return DoubleStream.of(hypotenouses).map(Math::sqrt).sum();
     }
